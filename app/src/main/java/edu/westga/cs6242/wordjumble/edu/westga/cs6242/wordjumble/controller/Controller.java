b3ee2452b65cff71f811742b222c590c31a5244a
R W Mullins <rmullin1@my.westga.edu>
2016-03-18 16:26:24 -0400

    Initial Framework.  Added Controller and Model sub packages for MVC.

---
+++ app/src/main/java/edu/westga/cs6242/wordjumble/edu/westga/cs6242/wordjumble/controller/Controller.java
@@ -0,0 +1,9 @@
+package edu.westga.cs6242.wordjumble.edu.westga.cs6242.wordjumble.controller;
+
+/**
+ * Created by Wayne Mullins on 3/18/2016
+ * @version 03/18/2016
+ */
+public class Controller {
+
+}//class Controller
---
+++ app/src/main/java/edu/westga/cs6242/wordjumble/edu/westga/cs6242/wordjumble/model/Model.java
@@ -0,0 +1,9 @@
+package edu.westga.cs6242.wordjumble.edu.westga.cs6242.wordjumble.model;
+
+/**
+ * Created by Wayne Mullins on 3/18/2016
+ * @version 03/18/2016
+ */
+public class Model {
+
+}//class Model

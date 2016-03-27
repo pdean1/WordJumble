f01bc4c760f42ab780760a32aaf91352fb39788a
pdean1 <pdean1@my.westga.edu>
2016-03-27 11:44:33 -0400

    Added methods associated with the word jumble game class.

--- app/src/main/java/edu/westga/cs6242/wordjumble/model/WordJumbleGame.java
+++ app/src/main/java/edu/westga/cs6242/wordjumble/model/WordJumbleGame.java
@@ -6,5 +6,39 @@ package edu.westga.cs6242.wordjumble.model;
  * This class represents a Word Jumble Game
  */
 public class WordJumbleGame extends edu.westga.cs6242.wordjumble.model.Model {
+    private String strCurrentWord;
+    private String strScrambledWord;
+    private String strUserAttempt;
 
+    /**
+     * Creates a new WordJumbleGame Object
+     */
+    public WordJumbleGame()
+    {
+        this.strCurrentWord = this.getRandomWord();
+        this.strScrambledWord = this.scrambleWord(this.strCurrentWord);
+        this.strUserAttempt = "";
+    }
+
+    /**
+     * Updates this user's attempt to solve the word jumble
+     * @param _strUserAttempt The updated user's attempt
+     */
+    public void updateUserAttempt(String _strUserAttempt)
+    {
+        this.strUserAttempt = _strUserAttempt;
+    }
+
+    /**
+     * Checks to see if the user has won the game.
+     * @return True if the game is over, false otherwise
+     */
+    public Boolean gameIsOver()
+    {
+        if (strUserAttempt.equals(strCurrentWord))
+        {
+            return true;
+        }
+        return false;
+    }
 }

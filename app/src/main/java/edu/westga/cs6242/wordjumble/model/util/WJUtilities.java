793d642979b69ced1bcb8918f2dc8e5fe7a8d15b
pdean1 <pdean1@my.westga.edu>
2016-03-29 13:44:44 -0400

    Hoping to use this class to pass the game object to the game activity; no functionality affected.

---
+++ app/src/main/java/edu/westga/cs6242/wordjumble/model/util/WJUtilities.java
@@ -0,0 +1,9 @@
+package edu.westga.cs6242.wordjumble.model.util;
+
+/**
+ * @author Patrick Dean
+ * @version 20160329
+ */
+public class WJUtilities {
+    public static final String NEW_GAME = "word_jumble.new_game";
+}

f057b5a07bd3ee3ae70d91c564956e72494a8d57
pdean1 <pdean1@my.westga.edu>
2016-03-29 12:36:15 -0400

    Adding the EWordLenght Enum.
    This enum will be passed to the WordGame Object to build a game for the
    user with a length of 5 or 6.

---
+++ app/src/main/java/edu/westga/cs6242/wordjumble/model/enums/EWordLength.java
@@ -0,0 +1,12 @@
+package edu.westga.cs6242.wordjumble.model.enums;
+
+/**
+ * For use currently with the Controller Class. This enum determines what length of word the user
+ * will play against
+ * @author Patrick Dean
+ * @version 20160329
+ */
+public enum EWordLength {
+    FIVE_LETTER_WORD, // str.length will equal 5
+    SIX_LETTER_WORD   // str.length will equal 6
+}

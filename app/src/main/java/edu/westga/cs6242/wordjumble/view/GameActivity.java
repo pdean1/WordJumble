ee90f2ea52971ea2ac9899a40d9c770dc546c9b1
pdean1 <pdean1@my.westga.edu>
2016-04-05 20:21:54 -0400

    Added toast buttons to the game view and wrote the onclick functions for them
    THIS IS THE FINAL COMMIT.

--- app/src/main/java/edu/westga/cs6242/wordjumble/view/GameActivity.java
+++ app/src/main/java/edu/westga/cs6242/wordjumble/view/GameActivity.java
@@ -1,14 +1,17 @@
 package edu.westga.cs6242.wordjumble.view;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.westga.cs6242.wordjumble.R;
 import edu.westga.cs6242.wordjumble.model.WordJumbleGame;
@@ -29,6 +32,9 @@ public class GameActivity extends AppCompatActivity {
         this.wjgGame = getIntent().getParcelableExtra(WJUtilities.NEW_GAME);
         this.initViewVariables();
         this.tvScrambledWord.setText(this.wjgGame.getStrScrambledWord());
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter.LengthFilter(this.wjgGame.getStrScrambledWord().length());
+        etUserInput.setFilters(filters);
     }
 
     public void click_AttemptToSubmitAnswer(View v)
@@ -65,6 +71,20 @@ public class GameActivity extends AppCompatActivity {
         this.etUserInput.setText("");
     }
 
+    public void click_DisplayHelp(View v) {
+        Context context = getApplicationContext();
+        String text = this.wjgGame.getStrCurrentWord();
+        String disp = "";
+        if (text.length() == 5) {
+            disp = text.substring(0,2);
+        }
+        else {
+            disp = text.substring(0, 3);
+        }
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, disp, duration);
+        toast.show();
+    }
 
     /// Private Helper Methods
 
